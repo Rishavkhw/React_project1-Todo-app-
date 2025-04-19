bacec8d269c99e20dcaa8233c6477b5bd6ee9421
rishav <rishabkhatiwada300@gmail.com>
2025-04-19 13:17:41 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  const a = "rishav";
+
+  return (
+    <div>
+      <h1>What is your name:</h1>
+      <h2>hello {a}</h2>
+    </div>
+  );
+}
+export default App;
+
+
+
